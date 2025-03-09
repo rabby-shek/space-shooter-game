151d7e197eb93c471d204f43847350a1e6f0deea
rabby-shek <rabbyshek1998@gmail.com>
2025-03-09 23:49:14 +0600

    background img and buttons responsiveness complete

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-class Enemy{
+class Enemy {
 
 }
 
-class Game{
+class Game {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+        window.addEventListener('resize', (e) => {
+            this.resize(e.target.innerWidth, e.target.innerHeight);
+        });
+    }
 
+    resize(width, height) {
+        this.width = width;
+        this.height = height;
+        this.canvas.width = width;
+        this.canvas.height = height;
+    }
 }
 
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    const game = new Game(canvas);
 });
