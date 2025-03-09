d965fdf606de515c0fa550bcd72214cc8868ec9f
rabby-shek <rabbyshek1998@gmail.com>
2025-03-09 23:56:19 +0600

    next day i will work on the enemy class

---
+++ enemy.js
@@ -0,0 +1,11 @@
+class Enemy {
+    constructor(game) {
+        this.game = game;
+        this.x;
+        this.y;
+        this.speedX;
+        this.speedY;
+        this.width = 50;
+        this.height = 50;
+    }
+}
--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-class Enemy {
 
-}
 
 class Game {
     constructor(canvas) {
